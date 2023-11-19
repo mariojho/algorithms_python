2c102c58c8f9894a7b93dead4ff70df90c63b60f
Mario <mario@linuxmex.com>
2023-11-19 22:21:43 +0100

    Adding area rectangulo

---
+++ areas/area_rectangulo.py
@@ -0,0 +1,8 @@
+''' Cálculo del área de un rectángulo '''
+
+area = 0; base = 0; altura = 0
+base = float(input("Dame la base del rectángulo: "))
+altura = float(input("Dame la altura del rectángulo: "))
+
+area = base * altura
+print("El área del rectángulo es: ", area, "\n")
