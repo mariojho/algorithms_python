c8dc68c7b156c1c53dfbed784e75409df8731623
Mario <mario@linuxmex.com>
2023-11-19 22:26:20 +0100

    Adding apotema cuadrado

---
+++ areas/apotema_cuadrado.py
@@ -0,0 +1,7 @@
+''' Cálculo apotema de un cuadrado '''
+apotema_cuadrado = 0; lado_cuadrado = 0
+
+lado_cuadrado = float(input("Dame el lado del cuadrado: "))
+apotema_cuadrado = lado_cuadrado / 2
+
+print("El apotema del cuadrado es: ", apotema_cuadrado, "\n")
