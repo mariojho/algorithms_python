64ea29e8fe4af052bc9cf424e94f58e0b4cfb95a
Mario <mario@linuxmex.com>
2023-11-19 22:36:57 +0100

    Adding area triangulo

---
+++ areas/area_triangulo.py
@@ -0,0 +1,8 @@
+''' Cálculo del área de un triángulo '''
+base_triangulo = 0; altura_triangulo = 0; area_triangulo = 0
+
+base_triangulo = float(input("Dame la base del triángulo: "))
+altura_triangulo = float(input("Dame la altura del triángulo: "))
+area_triangulo = (base_triangulo * altura_triangulo) / 2
+
+print("El área del triángulo es: ", area_triangulo, "\n")
